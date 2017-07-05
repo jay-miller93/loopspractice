8b174eb7e155b994e66e73b220b3ebfc0386e6b0
Jay <isaacsdaddy42293@gmail.com>
2017-07-05 15:15:45 -0400

    loops

--- loopspractice/Program.cs
+++ loopspractice/Program.cs
@@ -10,6 +10,20 @@ namespace loopspractice
     {
         static void Main(string[] args)
         {
+            //when you would use loop
+            //why you use loop
+            //nested loops
+            //infinite loops
+            //keywords break and continue
+
+            //for loop 
+            //can be used for both counting and iterating through an array(or other collection type)
+            //looks like:
+            //for(initializer; condition; updater)
+            //{
+            // do something
+             //}
+
         }
     }
 }
