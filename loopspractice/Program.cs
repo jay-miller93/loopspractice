6ac4a28e4ce38b37cb3f0776d1c8f732db9a3496
Jay <isaacsdaddy42293@gmail.com>
2017-07-06 10:11:41 -0400

    revised notes

--- loopspractice/Program.cs
+++ loopspractice/Program.cs
@@ -19,10 +19,26 @@ namespace loopspractice
             //for loop 
             //can be used for both counting and iterating through an array(or other collection type)
             //looks like:
+
             //for(initializer; condition; updater)
             //{
             // do something
-             //}
+            //}
+
+            //ex: for(int i = 0; i<=10; i++)  
+            //{ Console.WriteLine(i)}
+
+            
+            //for(int i = 15; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            for(int i = 50; i <=100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
 
         }
     }
